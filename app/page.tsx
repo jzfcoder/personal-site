ea2051ba3016bcd5d06fb1ade8b9bd705423f4d4
jzfcoder <jzflint@gmail.com>
2025-03-13 18:55:31 -0400

    mode fix

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,13 @@ export default function Home() {
   return (
     <div className='flex flex-col h-screen text-center items-center justify-center min-h-screen py-2 text-white'>
       <AsciiBackground />
-      <h1 className='text-3xl font-[family-name:var(--fira-header)]'>Jeremy Flint</h1>
-      <p className='font-[family-name:var(--fira-body)]'>Engineer. Entrepreneur. Explorer.</p>
+      <h1 className='text-6xl font-[family-name:var(--fira-header)]'>Jeremy Flint</h1>
+      <br />
+      <p className='flex flex-row space-x-4 font-[family-name:var(--fira-body)]'>
+        <span>Engineer.</span>
+        <span>Entrepreneur.</span>
+        <a href="zflint.com">Explorer.</a>
+      </p>
     </div>
   );
 }
