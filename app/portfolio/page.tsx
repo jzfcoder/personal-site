8a54a2e336d72d49b6a5cc50cb19bfb6613729b7
jzfcoder <jzflint@gmail.com>
2025-03-15 14:55:42 -0400

    portfolio update

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,43 +1,47 @@
 import AsciiBackground from "../AsciiBackground";
-
-type Project = {
-  title: string,
-  description: string,
-  imagePath: string,
-}
+import Projects from './Projects';
+import { Project } from "./Projects";
 
 type ProjectProps = {
   project: Project
 };
 
-function Project({ project }: ProjectProps) {
+function formatDate(date: Date) {
+  let arr = date.toLocaleDateString().split('/')
+  return arr.map((arg) => {
+    if (arg.length === 1) {
+      return `0${arg}`;
+    }
+    if (arg.length === 4) {
+      return `${arg[2]}${arg[3]}`;
+    }
+    return arg;
+  }).join('.')
+}
+
+function ProjectItem({ project }: ProjectProps) {
   return (
-    <div className='flex w-full max-w-2xl justify-center items-start p-4 my-3 min-w-[300px]'>
-      <img src={project.imagePath} alt={project.title} className='w-24 h-24 object-cover mr-6' />
+    <div className='relative flex w-full max-w-2xl justify-center items-start p-4 my-3 min-w-[300px] transform transition-transform duration-300 hover:scale-105 box cursor-pointer'>
+      {/* <img src={project.imagePath} alt={project.title} className='w-24 h-24 object-cover mr-6 mt-2' /> */}
       <div className='flex-1 text-left'>
-        <h1 className='text-xl font-[family-name:var(--fira-header)] mb-2'>{project.title}</h1>
-        <p className='font-[family-name:var(--fira-body)]'>{project.description}</p>
+        <div className='text-xl font-[family-name:var(--fira-header)] mb-2'>
+          {project.title}
+        </div>
+        <p className='font-[family-name:var(--fira-body)]'>
+          {formatDate(project.date)}
+          {} | {project.description}
+        </p>
       </div>
     </div>
   );
 }
 
 export default function Engineer() {
-  const projects: Project[] = [
-    {
-      title: 'Drone',
-      description: 'Braiding on Mars',
-      imagePath: 'portfolio/drone.png',
-    },
-    {
-      title: 'FTC',
-      description: 'Braiding on Mars',
-      imagePath: 'asdf',
-    },
-  ]
+  const projects: Project[] = Projects();
+
   return (
     <div className='flex flex-col h-screen text-center items-center justify-center min-h-screen py-2 text-white'>
-      <AsciiBackground 
+      <AsciiBackground
         cX={2}
         cY={2}
         rW={2}
@@ -45,18 +49,18 @@ export default function Engineer() {
         fR={1}
         inverse
       />
-      <div className='flex-1 flex flex-col items-center w-full p-5 md:p-20'>
-        <h1 className='text-5xl md:text-6xl font-[family-name:var(--fira-header)]'>Engineering Portfolio</h1>
+      <div className='flex-1 flex flex-col items-center w-full p-5 md:p-20 overflow-y-auto'>
+        <h1 className='text-5xl md:text-6xl font-[family-name:var(--fira-header)]'>Portfolio</h1>
+        <div className='font-[family-name:var(--fira-body)]'>
+          <span className='underline'><a href='/'>Return to Home</a></span>
+        </div>
         <br />
         <div className='w-full flex flex-col items-center'>
           {projects.map((project) => {
             return (
-              <Project project={project} key={project.title} />
+              <ProjectItem project={project} key={project.title} />
             );
           })}
-        </div>
-        <div className='font-[family-name:var(--fira-body)]'>
-            <span className='underline'><a href='/'>Return to Home</a></span>
         </div>
       </div>
     </div>
