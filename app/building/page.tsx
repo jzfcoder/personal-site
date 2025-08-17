7123db2332b3555b2843a485be8618b7574f98b1
jzfcoder <jzflint@gmail.com>
2025-08-17 15:15:38 -0700

    update current building

--- app/building/page.tsx
+++ app/building/page.tsx
@@ -12,7 +12,17 @@ export default function Engineer() {
         inverse
       />
       <div className='flex-1 flex-col w-3/4 p-5 md:p-20 min-w-100'>
-        <h1 className='text-5xl md:text-6xl font-[family-name:var(--fira-header)]'>Currently Building...</h1>
+        <h1 className='text-3xl font-[family-name:var(--fira-header)]'>Currently Building...</h1>
+        <br />
+        <div className='font-[family-name:var(--fira-body)]'>
+            <span className='underline'><a target="_blank" href='https://tryshortcut.ai'>Shortcut</a></span>
+            <br />
+            An AI agent for excel, at <a target="_blank" href='https://fundamentalresearchlabs.ai'>Fundamental Research Labs</a>
+        </div>
+        <br />
+        <br />
+        <br />
+        <h2 className='text-3xl font-[family-name:var(--fira-header)]'>=== Prev Built ===</h2>
         <br />
         <div className='font-[family-name:var(--fira-body)]'>
             <span className='underline'><a target="_blank" href='http://getclearify.com'>Clearify</a></span>
